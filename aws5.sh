af645cb501994577b71dcf0c1285f549c1d647af
Mohammadfazal-sayed <66768407+Mohammadfazal-sayed@users.noreply.github.com>
2020-08-24 19:22:56 +0800

    Update aws5.sh

--- aws5.sh
+++ aws5.sh
@@ -3,7 +3,7 @@ sudo chmod 777 -R /etc/nagios/
 sudo echo "allowed_hosts=127.0.0.1,172.31.63.150" >> /etc/nagios/nrpe.cfg '
 
 ssh ubuntu@172.31.63.150 '
-sudo chmod 777 -R /usr/local/nagios/servers/host1.cfg
+sudo chmod 777 -R /usr/local/nagios/servers/
 sudo echo " 
 define host {
         use                          linux-server
