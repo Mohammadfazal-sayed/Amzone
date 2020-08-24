5790d2e4b9b26fad886ef90a9d3e9a454fe0dafc
Mohammadfazal-sayed <66768407+Mohammadfazal-sayed@users.noreply.github.com>
2020-08-24 13:52:15 +0800

    Update aws5.sh

--- aws5.sh
+++ aws5.sh
@@ -1,5 +1,7 @@
-ssh ubuntu@172.31.50.174 '
+ssh ubuntu@$ip '
 sudo apt-get update
 sudo apt-get install nagios-nrpe-server nagios-plugins -y
+vim /etc/nagios/nrpe.cfg
+server_address=$ip
 sudo service nagios-nrpe-server restart
 '
